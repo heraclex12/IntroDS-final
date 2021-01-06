67402584b7a3b570d73872a34d01ad4e03c2d588
Ho Le <holl1@imt-soft.com>
2021-01-06 12:15:12 +0700

    - Clean up code

--- movieCrawling.py
+++ movieCrawling.py
@@ -13,64 +13,6 @@ browser.maximize_window()
 
 records = []
 
-
-def crawlAllComment(url):
-    records = []
-    browser.get(url)
-    # tat quang cao
-    browser.find_element_by_css_selector('html').send_keys(Keys.ESCAPE)
-
-    # scroll
-    time.sleep(1)
-    total_height = int(browser.execute_script(
-        "return document.body.scrollHeight"))
-    for i in range(1, total_height, 5):
-        browser.execute_script("window.scrollTo(0, {});".format(i))
-    time.sleep(3)
-
-    html_source = browser.page_source
-    soup = BeautifulSoup(html_source, 'html.parser')
-
-    # tat dialog
-    button = browser.find_elements_by_css_selector(
-        'button.align-right.secondary.slidedown-button')
-
-    if(len(button) > 0):
-        button[0].click()
-
-    html_source = browser.page_source
-    soup = BeautifulSoup(html_source, 'html.parser')
-
-    commentDivs = soup.findAll(
-        "div", {"class": "style__StyledComment-sc-103p4dk-5 dDtAUu review-comment"})
-    for commentDiv in commentDivs:
-        cmt = commentDiv.find(
-            "div", {"class": "review-comment__content"}).text
-        if(len(cmt) > 0):
-            records.append(cmt)
-
-    navigationButton = browser.find_elements_by_css_selector(
-        'a.btn.next')
-
-    while len(navigationButton) > 0:
-        navigationButton[0].click()
-        time.sleep(1)
-        html_source = browser.page_source
-        soup = BeautifulSoup(html_source, 'html.parser')
-        commentDivs = soup.findAll(
-            "div", {"class": "style__StyledComment-sc-103p4dk-5 dDtAUu review-comment"})
-        for commentDiv in commentDivs:
-            cmt = commentDiv.find(
-                "div", {"class": "review-comment__content"}).text
-            if(len(cmt) > 0):
-                records.append(cmt)
-        navigationButton = browser.find_elements_by_css_selector(
-            'a.btn.next')
-
-    return records
-# class="keywords right_column"
-
-
 def crawlMovieInfomation(url):
     browser.get(url)
     # tat quang cao
@@ -135,51 +77,7 @@ def crawlMovieInfomation(url):
     return {'url': url, 'title': title, 'score': user_score_chart, 'posterImagePath': imgFileName,
             'certification': certification, 'overview': overview, 'tagline': tagline, 'genres': genres, 'trailerUrl': trailerUrl}
 
-    # button = browser.find_elements_by_css_selector(
-    #     'div.filter-review__item')
-
-    # if(len(button) > 0):
-    #     button[3].click()
-    #     time.sleep(1)
-
-    #     html_source = browser.page_source
-    #     soup = BeautifulSoup(html_source, 'html.parser')
-
-    #     commentDivs = soup.findAll(
-    #         "div", {"class": "style__StyledComment-sc-103p4dk-5 dDtAUu review-comment"})
-    #     for commentDiv in commentDivs:
-    #         cmt = commentDiv.find(
-    #             "div", {"class": "review-comment__content"}).text
-    #         title = commentDiv.find(
-    #             "a", {"class": "review-comment__title"}).text
-    #         record = {'comment': title+', '+cmt, 'is_trust': 1}
-    #         print(record)
-    #         records.append(record)
-
-    #     button[3].click()
-    #     button[6].click()
-    #     button[7].click()
-    #     time.sleep(1)
-
-    #     html_source = browser.page_source
-    #     soup = BeautifulSoup(html_source, 'html.parser')
-
-    #     commentDivs = soup.findAll(
-    #         "div", {"class": "style__StyledComment-sc-103p4dk-5 dDtAUu review-comment"})
-
-    #     for commentDiv in commentDivs:
-    #         cmt = commentDiv.find(
-    #             "div", {"class": "review-comment__content"}).text
-    #         title = commentDiv.find(
-    #             "a", {"class": "review-comment__title"}).text
-    #         record = {'comment': title+', '+cmt, 'is_trust': 0}
-    #         print(record)
-    #         records.append(record)
-
-    # return records
-
-
-def getProductDetailUrl(url):
+def getMovieUrl(url):
     browser.get(url)
     # tat quang cao
     browser.find_element_by_css_selector('html').send_keys(Keys.ESCAPE)
@@ -193,11 +91,6 @@ def getProductDetailUrl(url):
 
     html_source = browser.page_source
     soup = BeautifulSoup(html_source, 'html.parser')
-
-    # movieUrl = 'https://www.themoviedb.org' + \
-    #     soup.find('div', {'id': 'trending_scroller'}).find(
-    #         'div').find('div').find('h2').find('a').get('href')
-    # return movieUrl
 
     time.sleep(1)
     t = 'document.body.scrollHeight'
@@ -225,54 +118,12 @@ def getProductDetailUrl(url):
         movieUrls.append(href)
     return movieUrls
 
-# productUrls = getProductDetailUrl('https://tiki.vn/deal-hot?tab=now&page=1')
-
-# fieldList = ['comment', 'is_trust']
-# try:
-#     with open('khdl_final_final6.csv', 'w', encoding='utf-8') as csvfile:
-#         writer = csv.DictWriter(csvfile, fieldnames=fieldList)
-#         writer.writeheader()
-#         for productUrl in productUrls:
-#             records = crawlComment(productUrl)
-#             for record in records:
-#                 print(record)
-#                 writer.writerow(
-#                     {'comment': record['comment'], 'is_trust': record['is_trust']})
-# except IOError:
-#     print("I/O error")
-# print(crawlComment('https://www.themoviedb.org/movie/464052-wonder-woman-1984'))
-
-
-def scroll(driver, timeout):
-    scroll_pause_time = timeout
-
-    # Get scroll height
-    last_height = driver.execute_script("return document.body.scrollHeight")
-
-    while True:
-        # Scroll down to bottom
-        driver.execute_script(
-            "window.scrollTo(0, document.body.scrollHeight);")
-
-        # Wait to load page
-        time.sleep(scroll_pause_time)
-
-        # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            # If heights are the same it will exit the function
-            break
-        last_height = new_height
-
-
-categoriesUrls = ['https://www.themoviedb.org/tv', 'https://www.themoviedb.org/tv/on-the-air', 'https://www.themoviedb.org/tv/top-rated',
-                  'https://www.themoviedb.org/movie', 'https://www.themoviedb.org/movie/now-playing', 'https://www.themoviedb.org/movie/top-rated']
-
-
+# categoriesUrls = ['https://www.themoviedb.org/tv', 'https://www.themoviedb.org/tv/on-the-air', 'https://www.themoviedb.org/tv/top-rated',
+#                   'https://www.themoviedb.org/movie', 'https://www.themoviedb.org/movie/now-playing', 'https://www.themoviedb.org/movie/top-rated']
 
 # urls = []
 # for url in categoriesUrls:
-#     urls += getProductDetailUrl(url)
+#     urls += getMovieUrl(url)
 # print(len(urls))
 
 # fieldList = ['id', 'movie_url']
